59ad6a5b28b8201eaee16ede9dfc296ebcffdd3a
NilminiDharmakeerthi94 <nilmini.dharmakeerthi@gmail.com>
2020-10-27 11:24:28 +0530

    Update books.php

--- app/api/books.php
+++ app/api/books.php
@@ -10,4 +10,14 @@ while($row = $result->fetch_assoc()){                         //Retieve data fro
 }
 echo json_encode($data);
 });
+
+$app->get('/api/books/{id}', function($request){
+    require_once('dbconnect.php');
+    $id = $request->getAttribute('id');               // Get book data by id
+    $query = "select * from books where id=$id";
+    $result = $mysqli->query($query);
+
+    $data[] = $result->fetch_assoc();
+   header('Content-type: application/json');
+    echo json_encode($data); 
 ?>
